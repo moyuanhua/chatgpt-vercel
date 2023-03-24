7db67b1daae8eeb9d5221f54f591832a653bddd8
moyuanhua <541237035@qq.com>
2023-03-24 03:50:03 +0000

    修改文案

--- src/system.ts
+++ src/system.ts
@@ -7,7 +7,7 @@ export const setting = {
   systemRule: ""
 }
 
-export const message = `未来就在你手中 Enjoy It
+export const message = `未来就在你手中 Enjoy It - murphy
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
