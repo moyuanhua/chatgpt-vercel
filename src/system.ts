090a775e34d41e77e923a4e39278675813763489
moyuanhua <541237035@qq.com>
2023-03-24 03:49:46 +0000

    修改文案

--- src/system.ts
+++ src/system.ts
@@ -7,7 +7,7 @@ export const setting = {
   systemRule: ""
 }
 
-export const message = `Powered by OpenAI Vercel
+export const message = `未来就在你手中 Enjoy It
 - [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。`
 
 export type Setting = typeof setting
